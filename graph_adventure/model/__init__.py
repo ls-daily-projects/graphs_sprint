9507bedc8f8b83dbbd9180cbbdbd97851067a0b8
Miikis <Miikis@users.noreply.github.com>
2019-11-09 21:08:42 -0800

    Add queue data structure
    Signed-off-by: Miikis <Miikis@users.noreply.github.com>

--- graph_adventure/model/__init__.py
+++ graph_adventure/model/__init__.py
@@ -1,3 +1,4 @@
 from .room_graph import RoomGraph
+from .queue import Queue
 
-__all__ = [RoomGraph]
+__all__ = [RoomGraph, Queue]
---
+++ graph_adventure/model/queue.py
@@ -0,0 +1,61 @@
+class QueueNode():
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Queue():
+
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.length = 0
+
+    def __str__(self):
+        output = []
+        current_node = self.first
+
+        if not current_node:
+            return "[]"
+
+        while current_node:
+            output.append(current_node)
+            current_node = current_node.next
+
+        return ", ".join([str(node) for node in output])
+
+    def enqueue(self, value):
+        new_node = QueueNode(value)
+
+        if self.length == 0:
+            self.first = new_node
+            self.last = new_node
+            self.length += 1
+            return self
+
+        previous_last = self.last
+        previous_last.next = new_node
+        self.last = new_node
+        self.length += 1
+        return self
+
+    def dequeue(self):
+        if self.length == 0:
+            return None
+
+        previous_first = self.first
+        self.first = previous_first.next
+        self.length -= 1
+        return previous_first
+
+    def peek(self):
+        return self.first
+
+    def clear(self):
+        self.first = None
+        self.last = None
+        self.length = 0
+        return self
